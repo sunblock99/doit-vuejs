9866536f9a9a498f715752f803037a745601c19c
sunblock99 <sunblock99@kakao.com>
2023-08-30 17:21:37 +0900

    투두리스트 작성

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" placeholder="투두를 등록해보아요 !" v-on:keyup.enter>
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+        </span>
+       
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            newTodoItem: ''
+        }
+    },
+    methods:{
+        addTodo(){
+            if(this.newTodoItem !== ""){
+                var value = this.newTodoItem && this.newTodoItem.trim();
+                localStorage.setItem(this.newTodoItem, this.newTodoItem);
+                this.clearInput();
+            }
+        },
+        clearInput(){
+            this.newTodoItem = '';
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: inline-block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+    <section>
+        <ul>
+            <li v-for="todoItems in todoItems">{{ todoItems }}</li>
+            
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            todoItems: []
+        }
+    },
+    created(){
+        if(localStorage.length > 0){
+            for(var i = 0; i < localStorage; i++){
+                this.todoItems.push(localStorage.key(i))
+            }
+        }
+    },
+
+}
+</script>
+
+<style>
+</style>

54c6935ee25621834f2ce006afbe449c29102ecb
sunblock99 <sunblock99@kakao.com>
2023-08-31 17:10:32 +0900

    github page를 사용해보자

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
+  
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './docs'),
+    publicPath: '/docs/',
     filename: 'build.js'
   },
   module: {
